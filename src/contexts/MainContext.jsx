a0b7b146f1a82a82a842b46c4dc8bd968cf5c37f
Aliev-cyber <Khabib.said7@gmail.com>
2023-07-25 12:27:33 +0600

    add

--- src/contexts/MainContext.jsx
+++ src/contexts/MainContext.jsx
@@ -1,12 +1,22 @@
+import axios from "axios";
 import React, { createContext, useContext } from "react";
+import { API } from "../utils/consts";
 
 const mainContext = createContext();
 export function useMainContext() {
   return useContext(mainContext);
 }
 
+async function addPods(newPod) {
+  try {
+    await axios.post(API, newPod);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 const MainContext = ({ children }) => {
-  const value = {};
+  const value = { addPods };
   return <mainContext.Provider value={value}>{children}</mainContext.Provider>;
 };
 
